884362eb30d93d3adfc6f61b2451802024184bdb
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2013-12-05 21:51:58 +0100

    Modeled block, free block and reserved block.

--- block.h
+++ block.h
@@ -3,6 +3,53 @@
 
 typedef int POSITION;
 
+class Block {
+protected:
+	bool reserved;
+	unsigned int size;
+public:
+	ReservedBlock(unsigned int size)
+	{
+		this.size = size;
+	}
 
+	bool isReserved() {
+		return reserved;
+	}
+
+	setSize(unsigned int size) {
+		this.size = size;
+	}
+
+	unsigned int getSize() {
+		return size;
+	}
+}
+
+class ReservedBlock: public Block {
+public:
+	ReservedBlock(unsigned int size): this(size)
+	{
+		reserved = true;
+	}
+}
+
+class FreeBlock: public Block {
+protected:
+	FreeBlock* next;
+public:
+	ReservedBlock(unsigned int size): this(size)
+	{
+		reserved = false;
+	}
+
+	setNext(FreeBlock* next) {
+		this.next = next;
+	}
+
+	FreeBlock* getNext() {
+		return next;
+	}
+}
 
 #endif // BLOCK_H
