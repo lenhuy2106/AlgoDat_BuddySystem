0d01c7fef8dbc7e761b49296b0e3171787539446
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2013-12-05 22:36:08 +0100

    Finished Heap class.

--- heap.cpp
+++ heap.cpp
@@ -1,9 +1,13 @@
 #include "heap.h"
-#include "math.h"
 
-Heap::Heap (int m) {
-    heapVec.reserve (m);
+Heap::Heap(int m) {
+    data = new int[m];
 }
 
-Heap::~Heap () {
+inline void Heap::SetVal(POSITION p, ATOM val) {
+	data[p] = val;
+}
+
+inline int Heap::GetVal(POSITION p) {
+	return data[p];
 }
--- heap.h
+++ heap.h
@@ -1,31 +1,18 @@
 #ifndef HEAP_H
 #define HEAP_H
-#define PSEUDO -1
-#include <vector>
-#include <iostream>
 
-using namespace std;
+#define PSEUDO -1
+
 typedef int POSITION;
 typedef int ATOM;
 
 class Heap {
-
+private:
+	int* data;
 public:
-    Heap (int m);
-    Heap (const Heap& ref);
-    Heap& operator = (const Heap& ref);
-    ~Heap ();
-    vector<int> heapVec;
-    ATOM val;
-
-    inline void SetVal (POSITION p, ATOM val) {
-
-        heapVec[p] = val;
-    }
-
-    inline int GetVal (POSITION p) {
-
-        return heapVec[p]; }
+    Heap(int m);
+    inline void SetVal(POSITION p, ATOM val);
+    inline int GetVal(POSITION p);
 };
 
 #endif // HEAP_H
