b8f48eadb9845c4864e134fc97e4c18d21b97fd4
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2013-12-05 22:40:26 +0100

    Basic structure of out test program.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,49 @@
 #include "buddysystem.h"
 
+#include <iostream>
 using namespace std;
 
-int main()
+BuddySystem* buddySystem = new BuddySystem(7);
+
+int main(int argc, const char* argv[])
 {
-    cout << "Hello World!" << endl;
+	/* TODO: fix
+	buddySystem->fListsVec[2].size();
 
-    BuddySystem* buddySystem = new BuddySystem(7);
-
-//    buddySystem->NewMem(2);
-    buddySystem->fListsVec[2].size();
-
-    for (int i = 0; i < buddySystem->heapVec.size (); i++)
+    for (int i = 0; i < buddySystem->heapVec.size (); i++) {
         cout << buddySystem->heapVec[i] << endl;
+	}
+	*/
 
+    char action;
+    do {
+        printOptions();
+        cout << "> ";
+        cin >> action;
+        doAction(action);
+    } while (action != 'q');
+    
     return 0;
+}
+
+void printOptions() {
+    cout << "Options:" << endl;
+    cout << "(a)llocate" << endl;
+	cout << "(n)ew memory" << endl;
+	cout << "(d)ispose memory" << endl;
+    cout << "(q)uit" << endl;
+}
+
+void doAction(char action) {
+    switch (action) {
+        case 'a':
+			// TODO
+            break;
+        case 'n':
+			// TODO
+            break;
+        case 'd':
+			// TODO
+            break;
+    }
 }
