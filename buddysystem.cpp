32924d40fb2206872dc6f5212b4be0e0bdea91ad
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2013-12-06 14:54:45 +0100

    BuddySystem: Separated implementation from header. BuddySystem gets a pointer to the heap in the constructor. It creates its own FreeLists object.

--- buddysystem.cpp
+++ buddysystem.cpp
@@ -1,3 +1,31 @@
 #include "buddysystem.h"
 
-BuddySystem::BuddySystem(int m) : FreeLists(m) {}
+BuddySystem::BuddySystem(Heap* heap) {
+	freeLists = new FreeLists(heap);
+}
+
+POSITION BuddySystem::Allocate (POSITION p) {
+    POSITION tmp;
+    for (tmp = p; freeLists->GetFromFree(tmp) < 0; tmp++) {
+    }
+    while (tmp != p) {
+        freeLists->AddToFree(tmp - 1);
+        tmp--;
+    }
+    freeLists->AddToFree(tmp);
+    return tmp;
+}
+
+POSITION BuddySystem::NewMem (POSITION p) {
+    POSITION tmp = p;
+    if (fListsVec[p].size () == 0) {
+        Allocate(tmp);
+//      catch arrayOutOfIndex!
+	}
+    heap->SetVal(p, 1);
+    return p;
+}
+
+void BuddySystem::DisposeMem (POSITION p) {
+	freeLists->AddToFree(p);
+}
--- buddysystem.h
+++ buddysystem.h
@@ -2,37 +2,15 @@
 #define BUDDYSYSTEM_H
 #include "freelists.h"
 
-class BuddySystem : public FreeLists {
-
+class BuddySystem {
+private:
+	Heap* heap;
+	FreeLists* freeLists;
 public:
-    BuddySystem (int m);
-
-    POSITION Allocate (POSITION p) {
-
-        POSITION tmp;
-        for (tmp = p; GetFromFree (tmp) < 0; tmp++) {
-        }
-        while (tmp != p) {
-            AddToFree (tmp - 1);
-            tmp--;
-        }
-        AddToFree (tmp);
-        return tmp;
-    }
-
-    POSITION NewMem (POSITION p) {
-
-        POSITION tmp = p;
-        if (fListsVec[p].size () == 0)
-            Allocate (tmp);
-//      catch arrayOutOfIndex!
-        heapVec[p] = 1;
-        return p;
-    }
-
-    void DisposeMem (POSITION p) {
-        AddToFree (p);
-    }
+    BuddySystem(Heap* heap);
+    POSITION Allocate(POSITION p);
+    POSITION NewMem(POSITION p);
+    void DisposeMem (POSITION p);
 };
 
 #endif // BUDDYSYSTEM_H
